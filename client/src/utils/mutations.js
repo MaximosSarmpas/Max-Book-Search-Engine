08b52e3ea607b1c47dcb08ea3d9909c0c36c444a
Maximos Sarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-05-04 15:07:57 -0400

    Update mutations.js
    comments added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,7 @@
+// Importing gql from the Apollo client library
 import { gql } from "@apollo/client";
 
+// Defining the LOGIN_USER mutation
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -12,6 +14,7 @@ export const LOGIN_USER = gql`
   }
 `;
 
+// Defining the ADD_USER mutation
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -24,6 +27,7 @@ export const ADD_USER = gql`
   }
 `;
 
+// Defining the SAVE_BOOK mutation
 export const SAVE_BOOK = gql`
   mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
@@ -42,6 +46,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
+// Defining the REMOVE_BOOK mutation
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
